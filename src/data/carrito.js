348f90946bb5d8070dd7e1e6028c49ded702417e
Piero Dev <pierodev21@gmail.com>
2024-09-24 23:04:14 -0500

    feat: finish cart project

---
+++ src/data/carrito.js
@@ -0,0 +1,101 @@
+export const carrito = [
+    {
+       "image": {
+            "thumbnail": "/carrito/images/image-waffle-thumbnail.jpg",
+            "mobile": "/carrito/images/image-waffle-mobile.jpg",
+            "tablet": "/carrito/images/image-waffle-tablet.jpg",
+            "desktop": "/carrito/images/image-waffle-desktop.jpg"
+       },
+       "name": "Waffle with Berries",
+       "category": "Waffle",
+       "price": 6.50
+    },
+    {
+        "image": {
+            "thumbnail": "/carrito/images/image-creme-brulee-thumbnail.jpg",
+            "mobile": "/carrito/images/image-creme-brulee-mobile.jpg",
+            "tablet": "/carrito/images/image-creme-brulee-tablet.jpg",
+            "desktop": "/carrito/images/image-creme-brulee-desktop.jpg"
+        },
+        "name": "Vanilla Bean Crème Brûlée",
+        "category": "Crème Brûlée",
+        "price": 7.00
+     },
+     {
+        "image": {
+            "thumbnail": "/carrito/images/image-macaron-thumbnail.jpg",
+            "mobile": "/carrito/images/image-macaron-mobile.jpg",
+            "tablet": "/carrito/images/image-macaron-tablet.jpg",
+            "desktop": "/carrito/images/image-macaron-desktop.jpg"
+        },
+        "name": "Macaron Mix of Five",
+        "category": "Macaron",
+        "price": 8.00
+     },
+     {
+        "image": {
+            "thumbnail": "/carrito/images/image-tiramisu-thumbnail.jpg",
+            "mobile": "/carrito/images/image-tiramisu-mobile.jpg",
+            "tablet": "/carrito/images/image-tiramisu-tablet.jpg",
+            "desktop": "/carrito/images/image-tiramisu-desktop.jpg"
+        },
+        "name": "Classic Tiramisu",
+        "category": "Tiramisu",
+        "price": 5.50
+     },
+     {
+        "image": {
+            "thumbnail": "/carrito/images/image-baklava-thumbnail.jpg",
+            "mobile": "/carrito/images/image-baklava-mobile.jpg",
+            "tablet": "/carrito/images/image-baklava-tablet.jpg",
+            "desktop": "/carrito/images/image-baklava-desktop.jpg"
+        },
+        "name": "Pistachio Baklava",
+        "category": "Baklava",
+        "price": 4.00
+     },
+     {
+        "image": {
+            "thumbnail": "/carrito/images/image-meringue-thumbnail.jpg",
+            "mobile": "/carrito/images/image-meringue-mobile.jpg",
+            "tablet": "/carrito/images/image-meringue-tablet.jpg",
+            "desktop": "/carrito/images/image-meringue-desktop.jpg"
+        },
+        "name": "Lemon Meringue Pie",
+        "category": "Pie",
+        "price": 5.00
+     },
+     {
+        "image": {
+            "thumbnail": "/carrito/images/image-cake-thumbnail.jpg",
+            "mobile": "/carrito/images/image-cake-mobile.jpg",
+            "tablet": "/carrito/images/image-cake-tablet.jpg",
+            "desktop": "/carrito/images/image-cake-desktop.jpg"
+        },
+        "name": "Red Velvet Cake",
+        "category": "Cake",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "/carrito/images/image-brownie-thumbnail.jpg",
+            "mobile": "/carrito/images/image-brownie-mobile.jpg",
+            "tablet": "/carrito/images/image-brownie-tablet.jpg",
+            "desktop": "/carrito/images/image-brownie-desktop.jpg"
+        },
+        "name": "Salted Caramel Brownie",
+        "category": "Brownie",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "/carrito/images/image-panna-cotta-thumbnail.jpg",
+            "mobile": "/carrito/images/image-panna-cotta-mobile.jpg",
+            "tablet": "/carrito/images/image-panna-cotta-tablet.jpg",
+            "desktop": "/carrito/images/image-panna-cotta-desktop.jpg"
+        },
+        "name": "Vanilla Panna Cotta",
+        "category": "Panna Cotta",
+        "price": 6.50
+     }
+]
