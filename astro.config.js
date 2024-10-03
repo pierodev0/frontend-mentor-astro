8c14a409587415eccd040c29551d808acd8f94ad
Piero Dev <pierodev21@gmail.com>
2024-10-03 04:38:22 -0500

    chore: disable base styles tailwind

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
-
+import tailwind from "@astrojs/tailwind";
 // https://vitejs.dev/config/
 export default defineConfig({
+    integrations: [
+        tailwind({
+          applyBaseStyles: false,
+        }),
+      ],
   
 })
