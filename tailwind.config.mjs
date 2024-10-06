67e6bd5c546967629afd346fc051d906a3363427
Piero Dev <pierodev21@gmail.com>
2024-10-06 17:02:08 -0500

    feat(manage-landing): creaet simplify section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"orange-manage": "hsl(12, 88%, 59%)",
+			}
+		},
 	},
 	plugins: [],
 }
